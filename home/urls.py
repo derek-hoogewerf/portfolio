22801ce0d45d9363fc47baf8d5b352c1dc90ca66
Derek Hoogewerf <dbhoogewerf@gmail.com>
2022-09-25 04:00:38 +0530

    app path to logs.html

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
+    path("log/", views.log_message, name="log"),
     path("home/<name>", views.hello_there, name="hello_there"),
 ]
